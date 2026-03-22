56dff86507abba784640409a6938c718654d1ebb
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-03-22 14:28:03 -0300

    Implementação dos métodos de adição e subtração na classe Fraction; atualização da entrada de frações no main.py

--- Trabalho1/Fracao/frac.py
+++ Trabalho1/Fracao/frac.py
@@ -14,21 +14,19 @@ class Fraction:
         if self.den == f2.den:
             return str(self.num + f2.num) + "/" + str(self.den)
         else:
-            return str("TODO: Calculo de adição com DEN diferente")
+            return str((self.num * f2.den) + (f2.num * self.den)) + "/" + str(self.den * f2.den)
+    
     # Método de subtração
     def __sub__(self, f2):
         if self.den == f2.den:
-            if self < f2:
-                # Caso do NUM de f2 ser maior que NUM de self
-                return "-" + str(self.num - f2.num) + "/" + str(self.den)
+            return str(self.num - f2.num) + "/" + str(self.den)
         else:
-            return str("TODO: Calculo de adição com DEN diferente")
-        
+            return str((self.num * f2.den) - (f2.num * self.den)) + "/" + str(self.den * f2.den)
     # Método de multiplicação
     def __mul__(self, f2):
         return str(self.num * f2.num) + "/" + str(self.den * f2.den)
-    # Método de divisão inteira
-    def __truediv__(self, f2):
-        
-        return str(self.num * f2.den) + "/" + str(self.den * f2.num)
     
+    # Método de divisão
+    def __truediv__(self, f2):
+        return str(self.num * f2.den) + "/" + str(self.den * f2.num)
+        #return Fraction(self.num * f2.den, self.den * f2.num)
