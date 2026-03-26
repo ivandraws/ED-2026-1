a77e124033d03f1da377f0c39874220b91cf6c12
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-03-26 09:20:16 -0300

    Implementação da lógica principal do jogo Vinte Um, incluindo regras, opções de início e fluxo de jogo

--- Trabalho1/VinteUm/main.py
+++ Trabalho1/VinteUm/main.py
@@ -1,6 +1,44 @@
 from baralho import Baralho
+from jogo import Jogo
+
+
 
 def main():
-    return
+
+    
+    print("----------Jogo do Vinte Um----------\n" \
+        "Regras: Para ganhar, tem que conseguir exatamente uma pontuação de 21\n" \
+        "Cada carta vale seu número base, com exceção do Valete, Rainha e Rei, que valem 10\n")
+    opt = int(input("Deseja começar o jogo ?\n1. Iniciar\n0. Sair\n"))
+    if opt == 1:
+        #iniciar jogo
+        Jooj = Jogo()
+    elif opt == 0:
+        return
+    
+    
+    while True:    
+        
+        player = Jooj.jogador
+        bar = Jooj.baralho
+        Jooj.distribuir()
+        
+        while True:
+            player.mostrarMão()
+            if escolha() == 1:
+                player.comprar(bar)
+            else:
+                break
+        Jooj.verificarVitória()    
+        
+        return
+            
+
+
+def escolha():
+    escol = int(input("Você quer\n1. Comprar\n2. Parar\n"))
+    return escol
+
 if __name__ == "__main__":
+    
     main()
