0eceb181465a94d39b0861aa0fa6a671a8933ad2
HederPereira <hederpereira37@gmail.com>
2026-05-03 22:51:55 -0300

    Refactor carregarArquivodeNomes to dynamically handle file reading and improve name storage

--- Trabalho3/arquivo.py
+++ Trabalho3/arquivo.py
@@ -3,7 +3,21 @@
 def carregarArquivodeNomes(diretorio: str):
     try:
         with open(diretorio, "r", encoding="utf-8") as f:
-            return f.read().splitlines()
+            size = 625
+            nomes = [None] * size
+            for i,nome in enumerate(f):
+                if i == size:
+                    aux = [None]*(size*2)
+                    for j in range(size):
+                        aux[j] = nomes[j]
+                    nomes = aux
+                    size *= 2
+
+                nomes[i]= nome.rstrip('\n')
+
+            print(nomes)
+            return nomes
+
     except Exception as e:
         print(f"Erro ao carregar: {e}")
         return []
@@ -15,3 +29,6 @@ def salvarOrdenados(nomes: list):
                 f.write(f"{nome}\n")
     except Exception as e:
         print(f"Erro ao salvar: {e}")
+
+
+carregarArquivodeNomes("/home/heder/Estudos/Estrutura de Dados/ED-2026-1/Trabalho3/nomes.txt")
