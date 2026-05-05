99eb85bf8ed5eefd9fb8d57aa2ffea9b9e593014
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-05-04 21:12:48 -0300

    Refactorado criarGrafico melhorando a renderização do gráfico de barras e o layout dos dois gráficos

--- Trabalho3/grafico.py
+++ Trabalho3/grafico.py
@@ -33,37 +33,39 @@ def criarGrafico(algoritimos, resultados):
     multiplier = 0
 
     fig, ax = plt.subplots(layout='constrained')
+    fig, ay = plt.subplots(layout='constrained')
 
     for attribute, measurement in resultados.items():
         offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute)
-        ax.bar_label(rects, padding=3)
+        
+        if multiplier == 0:
+            rects = ax.bar(x + offset, measurement, width, label=attribute)
+            ax.bar_label(rects, padding=3)
+        else:
+            rects = ay.bar(x + offset, measurement, width, label=attribute)
+            ay.bar_label(rects, padding=3)
         multiplier += 1
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Segundos(s)')
     ax.set_title('Comparação da eficiência dos algorítmos')
-    ax.set_xticks(x + width, algoritimos)
-    ax.legend(loc='upper left', ncols=3)
-    ax.set_ylim(0, 250)
+    ax.set_xticks(x, algoritimos)
+    ax.legend(loc='upper left', ncols=1)
+    ax.set_ylim(0, int(resultados["Tempo"][1]) * 4)
 
     print(resultados["Comparações"][0])
     print(resultados["Comparações"][1])
     print(resultados["Trocas"][0])
     print(resultados["Trocas"][1])
     
-    fig, ay = plt.subplots(layout='constrained')
+    
 
     #Refazer
-    for attribute, measurement in resultados.items():
-        offset = width * multiplier
-        rects = ay.bar(x + offset, measurement, width, label=attribute)
-        ay.bar_label(rects, padding=3)
-        multiplier += 1
+
 
     ay.set_ylabel('Quantidade')
     ay.set_title('Quantidade de comparações e trocas entre algorítimos de insertion')
-    ay.set_xticks(x + width, algoritimos)
-    ay.legend(loc='upper left', ncols = 3)
+    ay.set_xticks(x + width + 0.13, algoritimos)
+    ay.legend(loc='upper left', ncols = 2)
     ay.set_ylim(0, 1000000000)
     plt.show()
